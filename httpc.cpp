a93f76b67d73340ba60f4b143f572b7a8abc8c1a
Sourec <tothemyers@gmail.com>
2017-08-31 20:25:07 -0400

    Fix bug from hell

--- httpc.cpp
+++ httpc.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <regex>
-#include <sys/sendfile.h>
-#include <unistd.h>
 #include "cmdline.h"
 using std::cout;
 using std::cerr;
 using std::endl;
-using std::regex;
-using std::regex_match;
 
 #define MAX_BUF_SIZE 8192	//Apache default, might as well use it here
 #define HTTP_GET   	"GET /%s HTTP/1.1\r\n" \
@@ -73,13 +67,62 @@ int main(int argc, char* argv[]){
 
 	if (connect(netsocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) != 0){
 		perror("Failed to connect to server");
+		abort();
 	}
-	else if (args.verbose_given){
+	else if (args.verbose_given) {
 		cout << "Successfully connected to server\n" << endl;
+		printf("Sending request: \n");
+		printf(HTTP_GET, path, host);
+
+		/*
+    	 * ,-------,  ,  ,   ,-------,
+    	 *  )  ,' /(  |\/|   )\ ',  (
+    	 *   )'  /  \ (qp)  /  \  '(
+    	 *    ) /___ \_\/(_/ ___\ (
+    	 *     '    '-(   )-'    '
+    	 *            )w^w(
+    	 *            (W_W)
+    	 *             ((
+    	 *              ))					 |
+    	 *             ((					/|\
+    	 *              )  HERE BE DRAGONS //|\\
+		 *									 |
+		 *
+		 *
+		 * Those two lines of code above are the most magical printfs I have ever known, and I don't know why. They used to
+		 * be outside the else-if, until I went through this code cleaning up (the program was working flawlessly)
+		 * and realized that they were providing verbose output where there shouldn't be. So, I moved them to their
+		 * present home.
+		 *
+		 * Instant chaos. Segfaults *every single time.* So I moved them back. All is well. What the hell? They're
+		 * printfs! All I did is make them NOT run, why is that causing a segfault? Where is the segfault coming from,
+		 * anyways? It's... oh, wait, it's way down below, in the content length parser. What?!
+		 *
+		 * Ok, so something strange is going on. If I turn verbose output back on, no more segfaults. If I move one of
+		 * the printfs back outside again, no more segfaults either. Or, not always. Now it only segfaults *sometimes*.
+		 * Yay, non-determinism! Maybe this somehow messes with the IO stuff a tiny bit? What if I fflush stdout or even
+		 * the network socket? Nope, no difference. Check Wireshark... nope, server doesn't care either way, and the
+		 * requests going out are identical. Everything is the same network-wise.
+		 *
+		 * So I took a look at the actual segfault location in the code to try and figure it out. It's the weird
+		 * copy-buffer-to-temp line, so strtok doesn't eat my http data when I don't want it to. Maybe the content
+		 * length is getting garbled? ...nope. All is well. Maybe I'm overrunni- HOLY **** THAT'S IT
+		 *
+		 * I had forgotten that strcpy can cause buffer overflows if you don't do it right. I didn't think it would be
+		 * an issue because I was copying between buffers of the same size, but I guess it mattered; once I switched
+		 * over to strncpy, everything stopped segfaulting (and presumably the CNN.com servers breathed a sigh of relief
+		 * when I stopped hammering them with HTTP requests so I could test my non-deterministic bug).
+		 *
+		 * Bug fixed, right? Yup. Problem solved? Nope. I have no idea how that bug ever got revealed. Best I figure is
+		 * that the buffer was overflowing by 1 byte, but how that's related to a pair of printfs *not* executing is beyond
+		 * me. I've been writing C/C++ for about 5 years now, and this is just about the only code that has ever made me say
+		 * "WHAT THE F***?!" out loud. I know those printfs should probably be `cout`'s and not printfs, but
+		 * honestly, I'm too afraid to touch them. It took way too long to find and fix the first time through.
+		 */
 	}
-	printf("Sending request:\n");
-	printf(HTTP_GET, path, host);
 	dprintf(netsocket, HTTP_GET, path, host);
+
+
 
 	char buf[8192] = {'\0'};
 	if (read(netsocket, buf, MAX_BUF_SIZE) == -1){
@@ -88,17 +131,11 @@ int main(int argc, char* argv[]){
 
 	}
 
-	/* Since C/C++ REGEXES SUCK AND DON'T SUPPORT LOOKBEHINDS AND ARE NOT WORTHY OF USE,
-	 * I've implemented a very simple manual extraction method for the stupid content
-	 * length number.
-	 *
-	 * Curse you forever, regexcomp(). If only the CCC machines had Boost installed...
-	 */
-
+	//Extract the content length from the response, if there is one.
 	int contentLength = 0;
 	char tempBuf[MAX_BUF_SIZE];
 	if (strstr(buf, "Content-Length: ") != nullptr) {
-		char *lengthStr = strcpy(tempBuf, strstr(buf, "Content-Length: ")); //because strtok eats strings...
+		char *lengthStr = strncpy(tempBuf, strstr(buf, "Content-Length: "), MAX_BUF_SIZE); //because strtok eats strings...
 		lengthStr += strlen("Content-Length: ");
 		contentLength = atoi(strtok(lengthStr, "\r\n"));
 	}
@@ -111,14 +148,11 @@ int main(int argc, char* argv[]){
 		ssize_t st = read(netsocket, buf, MAX_BUF_SIZE);
 		if (st > 0){
 			printf(buf);
-			fflush(stdout);
 			printedBytes += st;
 		}
 		else
 			break;
-
-		//If we didn't read as much info as could fit into the buffer last time, check if the connection is still open.
-		//This is done by writing to the socket and seeing what happens. This feels wrong.
+		//sssh, read errors? what are those?
 	}
 
 	if (args.verbose_given)
